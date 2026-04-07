e82cf3bcae1592df5833494e29b65b1f08826faa
L'Hamza Deals <lhamza@deals.ma>
2026-04-07 17:36:53 +0100

    Cleanup: remove dead code, debug files, update gitignore

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,5 +2,3 @@ export { default as Navbar } from './Navbar';
 export { default as CategoryFilter } from './CategoryFilter';
 export { default as DealCard } from './DealCard';
 export { default as DealsGrid } from './DealsGrid';
-export { default as StatsBar } from './StatsBar';
-export { default as AdBanner, TopBannerAd, SidebarAd, InFeedAd } from './AdBanner';
